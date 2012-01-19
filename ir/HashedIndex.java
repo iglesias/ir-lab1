0c0957569e0fbb1e52d5f00a617f351746d09c07
iglesias <fernando.iglesiasg@gmail.com>
2012-01-19 11:04:25 +0100

    + lab skeleton

---
+++ ir/HashedIndex.java
@@ -0,0 +1,63 @@
+/*  
+ *   This file is part of the computer assignment for the
+ *   Information Retrieval course at KTH.
+ * 
+ *   First version:  Johan Boye, 2010
+ *   Second version: Johan Boye, 2012
+ */  
+
+
+package ir;
+
+import java.util.LinkedList;
+import java.util.HashMap;
+
+
+/**
+ *   Implements an inverted index as a Hashtable from words to PostingsLists.
+ */
+public class HashedIndex implements Index {
+
+    /** The index as a hashtable. */
+    private HashMap<String,PostingsList> index = new HashMap<String,PostingsList>();
+
+
+    /**
+     *  Inserts this token in the index.
+     */
+    public void insert( String token, int docID, int offset ) {
+	//
+	//  YOUR CODE HERE
+	//
+    }
+
+
+    /**
+     *  Returns the postings for a specific term, or null
+     *  if the term is not in the index.
+     */
+    public PostingsList getPostings( String token ) {
+	// 
+	//  REPLACE THE STATEMENT BELOW WITH YOUR CODE
+	//
+	return null;
+    }
+
+
+    /**
+     *  Searches the index for postings matching the query in @code{searchterms}.
+     */
+    public PostingsList search( LinkedList<String> searchterms, int queryType ) {
+	// 
+	//  REPLACE THE STATEMENT BELOW WITH YOUR CODE
+	//
+	return null;
+    }
+
+
+    /**
+     *  No need for cleanup in a HashedIndex.
+     */
+    public void cleanup() {
+    }
+}
