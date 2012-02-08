3ebad8f242fb63cb2e2f0cad43e30fba852a7b0f
iglesias <fernando.iglesiasg@gmail.com>
2012-02-08 09:17:43 +0100

    + termFreq attribute

--- ir/PostingsEntry.java
+++ ir/PostingsEntry.java
@@ -14,11 +14,32 @@ import java.util.LinkedList;
 public class PostingsEntry implements Comparable<PostingsEntry>, Serializable {
     
     public int docID;
+
     public double score;
 
     /**
+     *  Number of times the term appears in this document
+     */
+    public int termFreq;
+
+    /**
+     *  List with the positions of the term in the document docID
+     *  This information is required for positional indexes
+     */
+    public LinkedList< Integer > positions = new LinkedList< Integer >();
+
+    /**
+     *  Constructor
+     */
+    public PostingsEntry(int docID, int offset) {
+      this.docID    = docID;
+      this.termFreq = 1;
+      positions.add(offset);
+    }
+
+    /**
      *  PostingsEntries are compared by their score (only relevant 
-     *  in ranked retrieval).
+     *  in ranked retrieval)
      *
      *  The comparison is defined so that entries will be put in 
      *  descending order.
@@ -26,20 +47,6 @@ public class PostingsEntry implements Comparable<PostingsEntry>, Serializable {
     public int compareTo( PostingsEntry other ) {
 	return Double.compare( other.score, score );
     }
-
-    //
-    //  YOUR CODE HERE
-    //
-
-    // List with the positions of the term in the document docID. This
-    // information is required for positional indexes
-    public LinkedList< Integer > positions = new LinkedList< Integer >();
-
-    public PostingsEntry(int docID, int offset) {
-      this.docID = docID;
-      positions.add(offset);
-    }
-
 
 }
 
